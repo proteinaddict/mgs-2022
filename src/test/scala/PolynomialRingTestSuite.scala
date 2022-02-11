3aecd494dae54e6d9f2fb866b3a01707a5f542a2
Jim Newton <jnewton@lrde.epita.fr>
2022-02-11 14:44:23 +0100

    added polynomial test cases

--- src/test/scala/PolynomialRingTestSuite.scala
+++ src/test/scala/PolynomialRingTestSuite.scala
@@ -23,29 +23,117 @@ class PolynomialRingTestSuite extends AnyFunSuite {
 
   import PolynomialRing._
 
+  val max_num_tests = 1000
+
   type POLY = Map[Int,Double]
 
+  test("one"){
+    for{n <- 0 to 5
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        } {
+      assert(polyAlmostEqual(polyMultiply(polyOne,p),p))
+      assert(polyAlmostEqual(polyMultiply(p,polyOne),p))
+    }
+  }
+
+  test("zero"){
+    for{n <- 0 to 5
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        } {
+      assert(polyAlmostEqual(polyAdd(polyZero,p),p))
+      assert(polyAlmostEqual(polyAdd(p,polyZero),p))
+    }
+  }
+
   test("polyAlmostEqual"){
-    ???
+    assert(polyAlmostEqual(Map(),Map(),0.0))
+    assert(polyAlmostEqual(Map(1 -> 10.001),
+                           Map(1 -> 10.002),
+                           0.1))
+    assert(polyAlmostEqual(Map(1 -> 0.0,
+                               2 -> 10.0),
+                           Map(2 -> 10.0,
+                               3 -> 0.0),
+                           0.0))
+
+    for{n <- 0 to 5
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        } {
+      assert(polyAlmostEqual(p,p,0.0))
+    }
+
   }
 
   test("polyAdd"){
-    ???
+    for{n <- 0 to 5
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        q = randomPoly(n)
+        } {
+      assert(polyAlmostEqual(polyZero,polyAdd(p,polyScale(-1.0,p))))
+      assert(polyAlmostEqual(polyAdd(p,q),
+                             polyAdd(q,p)))
+    }
   }
 
-  test("polyScale"){
-    ???
+  test("polyScale") {
+    for {n <- 0 to 5
+         _ <- 0 to max_num_tests
+         p = randomPoly(n)
+         } {
+      assert(polyAlmostEqual(polyScale(1.0,p),
+                             p))
+      assert(polyAlmostEqual(polyAdd(polyScale(2.0,p),
+                                     polyScale(-3.0,p)),
+                             polyScale(-1.0,p)))
+    }
   }
 
-  test("polySubtract"){
-    ???
+  test("polySubtract") {
+    for {n <- 0 to 5
+         _ <- 0 to max_num_tests
+         p = randomPoly(n)
+         q = randomPoly(n)
+         } {
+      assert(polyAlmostEqual(polySubtract(polyScale(3.0, p),
+                                          polyScale(2.0, p)),
+                             p))
+      assert(polyAlmostEqual(polySubtract(p, q),
+                             polyScale(-1.0, polySubtract(q, p))))
+    }
   }
+
 
   test("polyMultiply"){
-    ???
+    for{n <- 0 to 5
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        q = randomPoly(n)
+        r = randomPoly(n)
+        } {
+      assert(polyAlmostEqual(p,polyMultiply(p,polyOne)))
+      assert(polyAlmostEqual(polyMultiply(p,q),
+                             polyMultiply(q,p)))
+      assert(polyAlmostEqual(polyMultiply(p,polyMultiply(q,r)),
+                             polyMultiply(polyMultiply(p,q),r)))
+    }
   }
 
   test("polyFastPower"){
-    ???
+    for{n <- 0 to 5
+        m <- 0 to 7
+        _ <- 0 to max_num_tests
+        p = randomPoly(n)
+        fast = polyFastPower(p,m)
+        slow = polySlowPower(p,m)
+        } {
+      assert(polyAlmostEqual(fast,
+                             slow,
+                             0.01),
+             s"m=$m expecting small difference: ${polySubtract(fast,slow)}")
+    }
   }
 }
