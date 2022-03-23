d1fd010c1a168fa8063fda4d93f016bc0b220b50
Jim Newton <jnewton@lrde.epita.fr>
2022-03-23 11:42:10 +0000

    fixed header

--- src/main/scala/AbstractAlgebra.scala
+++ src/main/scala/AbstractAlgebra.scala
@@ -1,4 +1,3 @@
-import scala.util.Random
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation
 // files (the "Software"), to deal in the Software without restriction,
@@ -17,6 +16,8 @@ import scala.util.Random
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import scala.util.Random
 
 object AbstractAlgebra {
   // TASK: take a sequence of strings and return the equivalence
