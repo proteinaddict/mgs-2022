4ef6be8264e00c2a3468934531d4e1f3580316f4
Jim Newton <jnewton@lrde.epita.fr>
2022-05-13 15:07:40 +0200

    updating tests

--- src/main/scala/functions/CountSubsets.scala
+++ src/main/scala/functions/CountSubsets.scala
@@ -3,10 +3,16 @@ package functions
 object CountSubsets {
 
   // TASK:
+  //   countFixedSizedSubsets(n,m)
   //      Use factorial to compute the number of n-sized subsets of an m-element set.
   //      Use your function to write some tests for the n-sized subset
   //      function which was developed in class.
-  def fixedSizedSubsets(n: Int, m: Int): Int = {
+  def countFixedSizedSubsets(superSetSize: Int, subSetSize: Int): Int = {
+    ???
+  }
+
+  // We implement this function in the lecture.
+  def fixedSizedSubsets[T](n:Int, objects:Set[T]):Set[Set[T]] = {
     ???
   }
 
