8a30858fc33d857ce6d1daa3f02a74f5e11de056
Yellow-Daisy <g.fike43@gmail.com>
2022-06-09 14:24:35 +0000

    +

--- src/main/scala/functions/Challenging Exercises.scala
+++ src/main/scala/functions/Challenging Exercises.scala
@@ -1,6 +1,19 @@
 package functions
 
 object ChallngingExercises2 {
-  def pythagoreamtriple(a: Int, b: Int, c: Int): Int = {}
-  def main(argv: Array[String]): Unit = {}
+  def pythagoreamtriple(m: Int, n: Int): List[List[Int]] = {
+    for { c <- (m to n).toList
+          b <- (m to n).toList
+          a <-  (m to n).toList
+          if (a*a + b*b == c*c)
+             if (a < b)
+               } 
+               yield List(a, b, c)
+              }
+
+
+  def main(argv: Array[String]): Unit = {
+    println(pythagoreamtriple(1, 613))
+
+  }
 }
