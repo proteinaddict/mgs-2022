b8e3ce039e462a50123f2339b43e320ef1652724
Jim Newton <jimka.issy@gmail.com>
2022-06-14 23:44:52 +0000

    Recursion

---
+++ src/main/scala/convergence/Recursion.scala
@@ -0,0 +1,24 @@
+package convergence
+
+object Recursion {
+  // sum of list of Int
+  def sumListOfInt(items: List[Int]): Int = {
+    ???
+  }
+
+  // product of a list of Double
+  def productListOfDouble(items: List[Int]): Int = {
+    ???
+  }
+
+  // sum a list of anything that is summable
+  def sumList[T](items: List[T], ident: T, add: (T, T) => T): T = {
+    ???
+  }
+
+  def harmonicSum(items: List[Double]): Double = {
+    ???
+  }
+
+  def main(argv: Array[String]): Unit = {}
+}
